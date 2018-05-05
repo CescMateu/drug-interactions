662a367b65dd08d2b78149af8c1f9637d40df47c
Cesc <cesc.mateu@gmail.com>
2018-05-05 13:42:09 +0200

    bio_tagger function updated. tests created

---
+++ bio_tagger.py
@@ -0,0 +1,74 @@
+import nltk
+
+def bio_tagger(text, drugs):
+    ''' (str, list of str) -> list of tuples
+    
+    Description: 
+    Given a sentence 'text' and a set of drugs 'drugs', this function returns a list of str that
+    contains a tag for each of the tokens in text. The tags can be either 'B', 'I' or 'O'. 'B' means
+    the token is the first part of a drug entity, 'I' means the token is the continuation of a drug entity,
+    and 'O' means that the token does not belong to a drug entity.
+    
+    Examples/Tests:
+    
+    >>> bio_tagger('Ibuprofeno is great!', ['Ibuprofeno'])
+    [('Ibuprofeno', 'B'), ('is', 'O'), ('great', 'O'), ('!', 'O')]
+    >>> bio_tagger('I would like to buy calcium-rich milk', ['calcium'])
+    [('I', 'O'), ('would', 'O'), ('like', 'O'), ('to', 'O'), ('buy', 'O'), ('calcium-rich', 'B'), ('milk', 'O')]
+    >>> bio_tagger('Give me TNF antioxidants together with sodium, please', ['TNF antioxidants', 'sodium'])
+    [('Give', 'O'), ('me', 'O'), ('TNF', 'B'), ('antioxidants', 'I'), ('together', 'O'), ('with', 'O'), ('sodium', 'B'), (',', 'O'), ('please', 'O')]
+    '''
+
+    # Preprocessing
+    # Tokenize all the words and elements of the original sentence
+    tokens = nltk.word_tokenize(text)
+    # Separate all the drugs into individual words
+    drugs = sum([word.split() for word in drugs],[])
+
+    # Bio Tagger
+    # Initialise the bio_tagged list that will accumulate the results and
+    # the 'prev_tag' variable
+    bio_tagged = []
+    prev_tag = 'O'
+
+    # For each token of the original sentence, we will check whether there is an entity contained in the token
+    # We will follow a very simple rules to tag each token with the 'BIO' system
+    for token in tokens:
+        if prev_tag == 'O' : # Begin NE or continue O
+
+            # In here we contemplate two different cases: 
+            # - Case 1: The token is exactly the same as one of the drugs list
+            # - Case 2: The token has a substring equal to a drug ('calcium-rich' case)
+            if any([drug in token for drug in drugs]):
+                bio_tagged.append((token,'B'))
+                prev_tag = 'B'
+            else:
+                bio_tagged.append((token,'O'))
+                prev_tag = 'O'
+
+        elif prev_tag == 'B': # Inside NE
+
+            if any([drug in token for drug in drugs]):
+                bio_tagged.append((token,'I'))
+                prev_tag = 'I'
+            else: 
+                bio_tagged.append((token,'O'))
+                prev_tag = 'O'
+
+        elif  prev_tag == 'I': # Inside NE
+
+            if any([drug in token for drug in drugs]):
+                bio_tagged.append((token,'I'))
+                prev_tag = 'I'
+            else: 
+                bio_tagged.append((token,'O'))
+                prev_tag = 'O'
+        
+    return(bio_tagged)
+
+
+            
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    
