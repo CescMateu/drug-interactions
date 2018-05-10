829d153a9d196d1188728a549f3503a668a6d6b8
Cesc <cesc.mateu@gmail.com>
2018-05-10 17:24:13 +0200

    Small bug fixed

--- drug_functions.py
+++ drug_functions.py
@@ -388,7 +388,7 @@ def createFeatureVector(sentence, drugbank_db):
     is_token_in_DrugBank_db = []
     for token in tokenized_sentence:
         for drug in drugbank_db:
-            if drug in token.islower():
+            if drug in token.lower():
                 is_token_in_DrugBank_db.append(1)
                 break
         else:
